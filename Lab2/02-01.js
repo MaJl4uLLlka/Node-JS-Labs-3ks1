6b348218dea70d9b3bc759c62c0989d74371993a
MaJl4uLLlka <70745048+MaJl4uLLlka@users.noreply.github.com>
2021-09-05 23:10:54 +0300

    02-01

---
+++ Lab2/02-01.js
@@ -0,0 +1,15 @@
+let http=require('http');
+let fs=require('fs');
+
+http.createServer((request, response)=>
+{
+    if(request.url==="/html")
+    {
+        let html=fs.readFileSync("./index.html");
+        response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+        response.end(html);
+    }
+}
+).listen(5000);
+
+console.log('Server running at http://localhost:5000/');
