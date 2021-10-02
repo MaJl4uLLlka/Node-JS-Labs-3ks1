9d2e0ef51bb6504b678d591b80c02021801465e0
MaJl4uLLlka <70745048+MaJl4uLLlka@users.noreply.github.com>
2021-10-02 19:54:15 +0300

    Lab4

--- Lab4/04-02.js
+++ Lab4/04-02.js
@@ -6,37 +6,53 @@ let data = require('./db');
 var db = new data.DB();
 
 db.on('GET',  (req,resp)=>{
-    console.log('DB.GET');
-    resp.end(JSON.stringify(db.select()));
+    setImmediate(() =>
+    {
+        console.log('DB.GET');
+        resp.end(JSON.stringify(db.select()));
+    })
 });
 
-db.on('POST', (req,resp)=>{console.log('DB.POST');
-    req.on('data', data=>{
+db.on('POST', (req,resp)=>{
+
+    setImmediate(() =>
+    {
+        console.log('DB.POST');
+        req.on('data', data=>{
         let r = JSON.parse(data);
         db.insert(r);
         resp.end(JSON.stringify(r));
-    });
+        });
+    })
 });
 
 db.on('PUT',  (req, resp) => {
-    console.log('DB.PUT');
-    req.on('data', data => {
+
+    setImmediate(() =>
+    {
+        console.log('DB.PUT');
+        req.on('data', data => {
         let r = JSON.parse(data);
         resp.end(JSON.stringify(db.update(r)) );
+        })
     })
 });
 
 db.on("DELETE",  (req, resp) => {
-    console.log('DB.DELETE');
-    
-    if( typeof url.parse(req.url, true).query.id != 'undefined')
+
+    setImmediate(() =>
     {
-        let id = parseInt(url.parse(req.url, true).query.id);
-        if(Number.isInteger(id))
+        console.log('DB.DELETE');
+    
+        if( typeof url.parse(req.url, true).query.id != 'undefined')
         {
-            resp.end( JSON.stringify(db.delete(id)) );
+            let id = parseInt(url.parse(req.url, true).query.id);
+            if(Number.isInteger(id))
+            {
+                resp.end( JSON.stringify(db.delete(id)) );
+            }
         }
-    }
+    })
 });
 
 
