8b53830bb58eb84a4e8c4a07088a09cc377f6e52
MaJl4uLLlka <70745048+MaJl4uLLlka@users.noreply.github.com>
2021-10-10 18:39:43 +0300

    Lab6

--- Lab6/06-02.js
+++ Lab6/06-02.js
@@ -2,6 +2,53 @@ const http = require('http');
 const fs   = require('fs');
 const url  = require('url');
 const nodemailer = require('nodemailer');
+const {google} = require('googleapis');
+const { oauth2 } = require('googleapis/build/src/apis/oauth2');
+
+const CLIENT_ID = '518136941350-gapp274035muo0eah50d3of1fd23d9ne.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//04fL_z0-EXbkuCgYIARAAGAQSNwF-L9IrhjhEl1mB4SpFwRHwJ4bxFqtIDo1a4iZVxBjUWfR-5YoTk8nA2uQQUd4dkrjjL2I9bGo';
+
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+
+async function sendMail()
+{
+    try {
+        const accesToken = await oAuth2Client.getAccessToken();
+
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: 'kantorkazimir@gmail.com',
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accesToken
+            }
+        });
+
+        const mailOptions = {
+            from: 'Kasimir Kantor <kantorkazimir@gmail.com>',
+            to:   'kantor.kazimir@outlook.com',
+            subject: "Hello from gmail using API",
+            text: 'Hello from Gmail using API',
+            html: '<h1>Hello from Gmail using API</h1>'
+        };
+
+        const result = await transport.sendMail(mailOptions);
+
+        return result;
+
+    } catch (error) {
+        return error;
+    }
+}
+
+
 
 http.createServer((request, response) => 
 {
@@ -18,7 +65,11 @@ http.createServer((request, response) =>
             break;
 
         case '/send':
-            response.end(body);
+            // req.on('data', data=>{
+            //     let info = JSON.parse(data);
+            //     });
+            sendMail().then(result => response.end('Email sent...'))
+            .catch(error => console.log(error.message));
             break;
 
         default:
